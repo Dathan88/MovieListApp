6ddf48a5a97752f22c3f4b2dd4efa1c67a45ac65
Dathan Spencer <dpspencer88@gmail.com>
2019-09-13 09:38:13 -0600

    Add get and delete to redux

---
+++ client/src/components/MovieList.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+import { connect } from 'react-redux';
+import { getMovies, deleteMovie } from '../actions/movieActions';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+	componentDidMount() {
+		this.props.getMovies();
+	}
+
+	onDeleteClick = id => {
+		this.props.deleteMovie(id);
+	};
+
+	render() {
+		const { movies } = this.props.movie;
+		return (
+			<Container>
+				<ListGroup>
+					<TransitionGroup className='movieList'>
+						{movies.map(({ id, name }) => (
+							<CSSTransition key={id} timeout={500} classNames='fade'>
+								<ListGroupItem>
+									<Button
+										className='remove-btn'
+										color='danger'
+										size='sm'
+										onClick={this.onDeleteClick.bind(this, id)}
+									>
+										&times;
+									</Button>
+									{name}
+								</ListGroupItem>
+							</CSSTransition>
+						))}
+					</TransitionGroup>
+				</ListGroup>
+			</Container>
+		);
+	}
+}
+
+MovieList.propTypes = {
+	getMovies: PropTypes.func.isRequired,
+	deleteMovie: PropTypes.func.isRequired,
+	movie: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+	movie: state.movie,
+});
+
+export default connect(
+	mapStateToProps,
+	{ getMovies, deleteMovie }
+)(MovieList);
