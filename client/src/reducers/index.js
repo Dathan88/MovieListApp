75f93e25ecc9261157abf9b900de6106f17f453f
Dathan Spencer <dpspencer88@gmail.com>
2019-09-18 12:30:37 -0600

    Create react + redux auth state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import movieReducer from './movieReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
 	movie: movieReducer,
+	error: errorReducer,
+	auth: authReducer,
 });
