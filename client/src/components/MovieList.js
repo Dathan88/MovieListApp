866090d4d6e3c37693e326de1d9026e716c8acc8
Dathan Spencer <dpspencer88@gmail.com>
2019-09-20 13:19:43 -0600

    Remove uuid import

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { getMovies, deleteMovie } from '../actions/movieActions';
 import PropTypes from 'prop-types';
