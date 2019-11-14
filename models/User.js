40f8c83d46fa68f3ccd396dcb69e96beecf40d64
Dathan Spencer <dpspencer88@gmail.com>
2019-11-14 10:21:37 -0700

    Ref Movie model for lists

--- models/User.js
+++ models/User.js
@@ -3,23 +3,24 @@ const Schema = mongoose.Schema;
 
 // CREATE SCHEMA
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    register_date: {
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	register_date: {
 		type: Date,
 		default: Date.now,
 	},
+	movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = User = mongoose.model('User', UserSchema);
